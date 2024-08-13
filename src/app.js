007ff6a60a3fa59b0e5983623f36ff3d1e50e8c7
Mayank Bavankar <mayanksahu1053@gmail.com>
2024-08-13 18:11:51 +0530

    feat: Added healthcheck, videos, comments, community, subscriptions, likes, playlists and dashboard routes

--- src/app.js
+++ src/app.js
@@ -29,8 +29,24 @@ app.use(cookieParser());
 
 // Routes import
 import userRouter from "./routes/user.routes.js";
+import videoRouter from "./routes/video.routes.js";
+import commentRouter from "./routes/comment.routes.js";
+import communityRouter from "./routes/community.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import liekRouter from "./routes/like.routes.js";
+import playlistRouter from "./routes/playlist.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js";
+import healthcheckRouter from "./routes/healthcheck.routes.js";
 
 // Routes declaration
+app.use("/api/v1/healthcheck", healthcheckRouter);
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/community", communityRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/likes", liekRouter);
+app.use("/api/v1/playlists", playlistRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
 
 export default app;
